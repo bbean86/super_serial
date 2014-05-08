93fc65237aa52da7c4f72ab42af4ed37efa7ff81
Ben Bean <bbean86@gmail.com>
2014-05-07 22:05:53 -0500

    Rename some methods, parameters, and variables and extract some methods

--- lib/super_serial/super_serialize.rb
+++ lib/super_serial/super_serialize.rb
@@ -16,6 +16,7 @@ module SuperSerial #like, for srs
     # USAGE EXAMPLES
     #
     # class AwesomeClass < ActiveRecord::Base
+    #   include SuperSerial
     #   super_serialize :super_column, entry_1: 'SOME DEFAULT VALUE', entry_2: nil, entry_3: true
     # end
 
@@ -37,22 +38,27 @@ module SuperSerial #like, for srs
     private
       def handle_entry(entry)
         entry.each_pair do |entry_name, default_value|
-          define_getters_and_setters(entry_name)
+          define_getter_and_setter(entry_name)
           attr_accessible(entry_name)
-
-          if !!default_value == default_value # is a boolean
-            class_eval <<-RUBY, __FILE__, __LINE__ +1
-                        def #{ entry_name }?
-                          #{ entry_name }
-                        end
-            RUBY
-          end
+          define_boolean_accessor(entry_name) if !!default_value == default_value # is a boolean
         end
-        self.send(:before_validation, proc { set_entry_default(entry) }, {on: :create})
-        self.send(:before_validation, proc { check_serialized_data_types(entry) })
+        set_callbacks(entry)
       end
 
-      def define_getters_and_setters(entry)
+      def define_boolean_accessor(entry_name)
+        class_eval <<-RUBY, __FILE__, __LINE__ +1
+              def #{ entry_name }?
+                #{ entry_name }
+              end
+        RUBY
+      end
+
+      def set_callbacks(entry)
+        self.send(:before_validation, proc { set_entry_default(entry) }, { on: :create })
+        self.send(:before_validation, proc { check_serialized_data_type(entry) })
+      end
+
+      def define_getter_and_setter(entry)
         class_eval <<-RUBY, __FILE__, __LINE__ +1
                 def #{ entry }
                   #{ column_name }.#{ entry }
@@ -72,23 +78,35 @@ module SuperSerial #like, for srs
       end
     end
 
-    def check_serialized_data_types(entry)
+    def check_serialized_data_type(entry)
       entry.each_pair do |entry_name, default_value|
-        if default_value.present?
-          default_class_name = get_friendly_default_class(default_value)
-          errors.add(:base, "#{ entry_name.to_s } can only be stored as a #{ default_class_name.downcase }") unless entry_type_valid?(default_class_name, entry_name)
-        end
+        validate_value_type(default_value, entry_name) if default_value.present?
       end
       errors.empty?
+    end
+
+    def validate_value_type(default_value, entry_name)
+      default_value_class = get_friendly_default_class(default_value)
+      entry_value = send(entry_name)
+
+      unless value_type_valid?(default_value_class, entry_value, entry_name)
+        errors.add(:base, "#{ entry_name.to_s } can only be stored as a #{ default_value_class.downcase }")
+      end
     end
 
     def get_friendly_default_class(default_value)
       get_friendly_class_name(default_value.class.name)
     end
 
-    def entry_type_valid?(default_class_name, entry_name)
-      entry_class_name = get_friendly_class_name(send(entry_name).class.name)
-      entry_class_name == default_class_name || entry_can_be_converted?(default_class_name, entry_name)
+    def value_type_valid?(default_value_class, value, entry_name)
+      value_class_name = get_friendly_class_name(value.class.name)
+      value_class_name == default_value_class || value_can_be_converted?(default_value_class, value, entry_name)
+    end
+
+    def value_can_be_converted?(default_value_class, value, entry_name)
+      symbolized_default_class = default_value_class.downcase.to_sym
+      converted_value          = cast_value_to_default(value, symbolized_default_class)
+      try_conversion(converted_value, entry_name)
     end
 
     CONVERSIONS = {
@@ -96,18 +114,12 @@ module SuperSerial #like, for srs
         float: :to_f
     }
 
-    def entry_can_be_converted?(default_class_name, entry_name)
-      symbolized_class_name = default_class_name.downcase.to_sym
-      converted_value       = cast_value_for_entry(entry_name, symbolized_class_name)
-      try_conversion(converted_value, entry_name)
-    end
-
-    def cast_value_for_entry(entry_name, default_class_type)
+    def cast_value_to_default(value, default_class_type)
       if default_class_type == :boolean
-        cast_to_boolean(send(entry_name))
+        cast_to_boolean(value)
       else
         return unless CONVERSIONS.has_key?(default_class_type)
-        send(entry_name).try(CONVERSIONS[default_class_type])
+        value.try(CONVERSIONS[default_class_type])
       end
     end
 
@@ -122,7 +134,7 @@ module SuperSerial #like, for srs
     end
 
     def set_entry_value(value, entry_name)
-      send("#{entry_name}=", value)
+      send("#{ entry_name }=", value)
       send(entry_name) == value
     end
 
